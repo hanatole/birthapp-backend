44da3b574ca3224f763f5a11aefd55b3b95641f3
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-12 17:46:15 +0100

    Project initialization

---
+++ src/test/java/com/bitnoises/birthapp/BirthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitnoises.birthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
